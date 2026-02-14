b5fe05f09bb59847b9fb7d508d5989a66968e4f3
Bartek Wons <bartek.wons@gmail.com>
2026-02-14 12:15:59 +0100

    add cors

--- FinWallet/FinWallet/Program.cs
+++ FinWallet/FinWallet/Program.cs
@@ -9,6 +9,14 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        p => p.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
